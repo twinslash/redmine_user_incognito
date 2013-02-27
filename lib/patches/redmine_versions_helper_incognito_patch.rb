d75f8d2fdd3e1358a01456e121c14b420025423f
Dmitriy Brodnitskiy <dmitriy.brodnitskiy@twinslash.com>
2013-02-27 13:58:47 +0300

    compatibility with custom_overview

--- lib/patches/redmine_versions_helper_incognito_patch.rb
+++ lib/patches/redmine_versions_helper_incognito_patch.rb
@@ -8,7 +8,14 @@ module RedmineVersionsHelperIncognitoPatch
 
   module InstanceMethods
     def status_by_options_for_select_with_incognito(value)
-      if User.current.allowed_to?(:no_show_names, Version.find(params[:id]).project) && !User.current.admin?
+
+      if params[:controller] == 'projects'
+        project = Project.find(params[:id])
+      elsif params[:controller] == 'versions'
+        project = Version.find(params[:id]).project
+      end
+
+      if User.current.allowed_to?(:no_show_names, project) && !User.current.admin?
         options_for_select(VersionsHelper::STATUS_BY_CRITERIAS.reject{|s| s == 'author' || s == 'assigned_to'}.collect {|criteria| [l("field_#{criteria}".to_sym), criteria]}, value)
       else
         status_by_options_for_select_without_incognito(value)
